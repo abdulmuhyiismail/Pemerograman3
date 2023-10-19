4fd98bc3bdcba41a6971758fc4cdfbf74d9b94dd
abdulmuhyiismail <114898486+abdulmuhyiismail@users.noreply.github.com>
2023-10-19 21:23:06 +0700

    Add files via upload

---
+++ array ami.js
@@ -0,0 +1,15 @@
+const restaurant = [
+    {
+      name: "kedai kopi",
+      city: "aceh",
+      favoriteDrink: "kopi mix",
+      favoriteFood: "kopi original",
+      isVegan: true,
+    },
+  ];
+  
+  console.log("Nama : " + restaurant[0].name);
+  console.log("Kota : " + restaurant[0].city);
+  console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+  console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+  console.log("Vegan : " + restaurant[0].isVegan);
---
+++ variable ami.js
@@ -0,0 +1,14 @@
+const restaurant = {
+    name: "kedai kopi",
+    city: "aceh",
+    favoriteDrink: "kopi mix",
+    favoriteFood: "kopi original",
+    isVegan: false,
+  };
+  
+  const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+  console.log("Nama : " + name);
+  console.log("Kota : " + city);
+  console.log("Minuman Favorit : " + favoriteDrink);
+  console.log("Makanan Favorit : " + favoriteFood);
+  console.log("Vegan : " + isVegan);
